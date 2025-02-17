fb4e0f6c50295f85445f557c807ed53329b3ce72
Ik Ben <40415727+Ikxyz@users.noreply.github.com>
2025-02-17 07:13:33 +0100

    bug fix

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -6,20 +6,12 @@ import { getTemplate } from '../templates/template'
 export const maxDuration = 30 // Allow streaming responses up to 30 seconds
 
 const template = getTemplate()
+const RULE = process.env.RULE;
 
 const getContent = (context: string, question: string) => `
-  'You are an AI assistant who knows everything about Movement Blockchain and other web3 crypto technologies called MoveAi.
-  Use the below context to augment what you know about Movement Blockchain.
-  Don't use  solidity, make us of movement language. expect when asked explicitly asked for it.
-  Don't use aptos and solidity for any thing deployment use movement language instead. expect when asked explicitly asked for it.
-  When ever the user send a message saying or greeting using the word "gmove" respond with "wassa wassa" this is very important.
-  When generating movement module generate it as an aptos movement module this is very important.
-  By default any question asked should be treated as a question about Movement Blockchain including code generation, smart contract development, and other related topics.
-  The context will provide you with the most recent page data from movementnetwork.xyz, the official movement  website and others.
-  If the context doesn't include the information you need answer based on your existing knowledge and don't mention the source of your information or what the context does or doesn't include.
-  Format responses using markdown where applicable and don't return images.
+  '
 
-
+${RULE}
 
   ------------------------
   START CONTEXT
